2c3a53aff7b17e90ab26745dbf866837e2a1bfcc
Chondan Susuwan <chondansusuwan@outlook.com>
2025-09-27 01:51:33 +0700

    Setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '@redux/store';
+import { increment, decrement, fetchValue, fetchText, double } from '@redux/slices/exampleSlice';
+import clsx from 'clsx';
+import { config } from '@config/index';
 
 function App() {
+    const value = useSelector((state: RootState) => state.example.value);
+    const loading = useSelector((state: RootState) => state.example.loading);
+    const text = useSelector((state: RootState) => state.example.text);
+    const textLoading = useSelector((state: RootState) => state.example.textLoading);
+    const dispatch = useDispatch<AppDispatch>();
+
     return (
         <div className='App'>
             <header className='App-header'>
@@ -13,6 +24,25 @@ function App() {
                 <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
                     Learn React
                 </a>
+                <div style={{ marginTop: 20 }}>
+                    <h3>Redux Example Value: {value}</h3>
+                    <button onClick={() => dispatch(increment())}>Increment</button>
+                    <button onClick={() => dispatch(double())} style={{ marginLeft: 8 }}>
+                        Double
+                    </button>
+                    <button onClick={() => dispatch(decrement())} style={{ marginLeft: 8 }}>
+                        Decrement
+                    </button>
+                    <button onClick={() => dispatch(fetchValue())} style={{ marginLeft: 8 }} disabled={loading}>
+                        {loading ? 'Loading...' : 'Fetch Random Value'}
+                    </button>
+                </div>
+                <div style={{ marginTop: 20 }}>
+                    <h3>Redux Example Text: {text}</h3>
+                    <button onClick={() => dispatch(fetchText({ text: 'Hi' }))} disabled={textLoading}>
+                        {textLoading ? 'Loading...' : 'Fetch Text'}
+                    </button>
+                </div>
             </header>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from '@src/routes/AppRouter';
+import { Provider } from 'react-redux';
+import { store } from '@src/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <AppRouter />
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
     </React.StrictMode>
 );
 
